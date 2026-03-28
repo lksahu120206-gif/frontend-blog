472d6f0af4dbdab36ef81aa770944b1554f74c9f
Lalit Kishor Sahu <lksahu120206@gmail.com>
2026-03-28 23:45:56 +0530

    fix: React Router future flags, remove duplicate ToastContainer

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <AuthProvider>
         <App />
-        <ToastContainer position="top-right" theme="dark" />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
